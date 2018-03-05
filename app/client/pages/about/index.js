32a0cada1e9cddc848389cbeaf901a6338c646a8
howell5 <howell95@outlook.com>
2018-03-06 00:26:40 +0800

    add about

---
+++ app/client/pages/about/index.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class About extends Component {
+  render() {
+    return (
+      <div>
+        <h2>hello this is howell</h2>
+      </div>
+    );
+  }
+}
+
+export default About;
---
+++ app/client/pages/home/index.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import './style.less';
+
+class Home extends Component {
+  render() {
+    return (
+      <div className="home">
+        <div className="menu">
+          <button className="toggle" href="#">
+            <i className="change" />
+          </button>
+        </div>
+        <div className="heading">
+          <h2>howell</h2>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Home;
